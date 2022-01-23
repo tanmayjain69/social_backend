8db2e43363e378e45fb6cd8e829455be0cf4acb2
tanmayjain69 <tj2023@bennett.edu.in>
2022-01-23 15:29:21 +0530

    fix

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,8 +3,8 @@ dotenv.config();
 module.exports = {
   HOST: process.env.PG_HOST,
   USER: process.env.PG_USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: 'testdb',
+  PASSWORD: process.env.PG_PASSWORD,
+  DB: process.env.PG_DB,
   dialect: 'postgres',
   pool: {
     max: 5,
